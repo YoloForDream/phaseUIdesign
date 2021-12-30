e026eeaeb74ee0097857bd8371a06c0287b0eaf5
RWang18 <wangwei19900921@hotmail.com>
2021-12-30 18:54:39 +0800

    完成该Phase的页面布局以及逻辑设计

--- src/com/rockwell/Innovent/phase/ui/gbn0100.java
+++ src/com/rockwell/Innovent/phase/ui/gbn0100.java
@@ -1,21 +1,30 @@
 package com.rockwell.Innovent.phase.ui;
 
+import com.datasweep.compatibility.client.*;
+import com.datasweep.plantops.common.dataobjects.DUserSequenceValue;
 import com.jgoodies.common.internal.IActionBean;
 import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.debug.FormDebugPanel;
 import com.jgoodies.forms.factories.CC;
 import com.jgoodies.forms.layout.FormLayout;
+import com.rockwell.mes.apps.ebr.ifc.phase.ui.PhaseErrorDialog;
 import com.rockwell.mes.apps.ebr.ifc.phase.ui.UIConstants;
 import com.rockwell.mes.apps.ebr.ifc.swing.PhaseColumnLayout;
+import com.rockwell.mes.commons.base.ifc.services.PCContext;
 import org.apache.ecs.vxml.Return;
-
+import com.datasweep.compatibility.pnutsfunctions.utility.println;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Objects;
+
+
 
 public class gbn0100<selectBatchtype> extends JFrame {
 
@@ -23,12 +32,19 @@ public class gbn0100<selectBatchtype> extends JFrame {
     private JTextField batchNumber;
     private JComboBox  selectBatchtype;
     private JButton generateBatchnumber;
+    private Date date;//用于获取当前日期
+    private String sequencenameUsedforwc;//待清洗批号的流水号
+    private String sequencenameUsedfocb;//清洗批号的流水号
+    private String sequencenameUsedfosb;//灭菌批号的流水号
+    private String sequencenameUsedfoab;//组装批号的流水号
+    String sequenceResult;
     ActionEvent e;
 
     public gbn0100() throws HeadlessException{
 
         init();
         layoutComponents();
+        //intiAllsequencenumber();
     }
     private void layoutComponents(){
 
@@ -86,7 +102,7 @@ public class gbn0100<selectBatchtype> extends JFrame {
 
     }
     public  void init(){
-
+        date = new Date();
         String str1 = "--Please select type of batch number--";
         String str2 = "Wating for cleaning bathch number";
         String str3 = "Cleaning batch number";
@@ -124,34 +140,125 @@ public class gbn0100<selectBatchtype> extends JFrame {
             public void actionPerformed(ActionEvent e) {
 
                 generateBatchnumberbytype(selectBatchtype.getSelectedItem().toString());
+
                 //System.out.println("你按下了" + btn.getText());
             }
         });
+    }
+
+
+    public UserSequence createSquencename(String sequenceName) {
+        UserSequence sequence = PCContext.getFunctions().createUserSequence(sequenceName);
+        sequence.setInitialValue(0);
+        sequence.save();
+        return sequence;
 
     }
+    public UserSequence resetSquencenumber(String sequenceName) {
+        UserSequence sequence = PCContext.getFunctions().createUserSequence(sequenceName);
+        sequence.resetSequence("Reset Per Month");
+        return sequence;
+    }
+
+
+    public UserSequence checkSquencename(String sequenceName){
+       // sequencenameUsedforwc="watingForclen";
+        //Object result ;
+        UserSequence  sequenceObj =  PCContext.getFunctions().getUserSequenceByName(sequenceName);
+
+        return sequenceObj ;
+    }
+    public void intiAllsequencenumber(){
+
+        sequencenameUsedforwc = getSquencenumber("watingForCleaning",isFirstdayofmonth(date));
+        sequencenameUsedfocb =  getSquencenumber("cleaningBatchnumber",isFirstdayofmonth(date));
+        sequencenameUsedfosb =  getSquencenumber("steriLizationbatchnumber",isFirstdayofmonth(date));
+        sequencenameUsedfoab =  getSquencenumber("assemblyBatchnumber",isFirstdayofmonth(date));
+    }
+
+    public String getSquencenumber(String sequenceName, boolean isFirstdayofMaonth) {
+
+        UserSequence sequence = checkSquencename(sequenceName);//验证该类型的流水对象是否存在
+        String sequenceResult = null;
+        if(Objects.nonNull(sequence)&&!isFirstdayofMaonth){
+
+            UserSequenceValue object = (UserSequenceValue)sequence.getNextValue().getResult();
+            sequenceResult = String.valueOf(object.getValue());
+        }
+        else if(Objects.isNull(sequence))//如果系统中不存在改sequence，则先创建
+        {
+            sequence = createSquencename(sequenceName);
+            @SuppressWarnings("null")
+            UserSequenceValue object = (UserSequenceValue) sequence.getNextValue().getResult();
+            sequenceResult = String.valueOf(object.getValue());
+        }
+        else if(isFirstdayofMaonth)//如果系统中存在该Sequence,但是系统当前时间为当月第一天，则需要重置计数
+        {
+            sequence= resetSquencenumber(sequenceName);
+            UserSequenceValue object = (UserSequenceValue) sequence.getNextValue().getResult();
+            sequenceResult = String.valueOf(object.getValue());
+        }
+        if (Integer.parseInt(sequenceResult) <= 9)
+        {
+            sequenceResult="0"+sequenceResult;
+        }
+
+        return sequenceResult;
+    }
+
+
+    public boolean isFirstdayofmonth(Date date){
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+       // int t = calendar.DAY_OF_MONTH
+        return  calendar.get(calendar.DAY_OF_MONTH)==1;
+    }
+
     public void generateBatchnumberbytype(String batchType) {
 
+        batchNumber.setText("");
         String type = batchType;
         SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyMM");
         SimpleDateFormat dateFormat2 = new SimpleDateFormat("yyyyMM");
-        Date date = new Date();
+
 
         switch (type) {
             case "Wating for cleaning bathch number":
                 batchNumber.setText("M2-DHT" + dateFormat1.format(date));
             break;
             case "Cleaning batch number":
-                if(equipmentId.getText()!=null){
-                batchNumber.setText("M2"+"-"+equipmentId.getText()+"-" + dateFormat1.format(date));}
+                if(!equipmentId.getText().isEmpty()){
+
+                    batchNumber.setText("M2"+"-"+equipmentId.getText()+"-" + dateFormat1.format(date));
+                }
+                else {
+                    PhaseErrorDialog errorDialog = new PhaseErrorDialog();
+                    errorDialog.showDialog("The Euqipment ID is null,please check it and try aganin!");
+                    //JOptionPane.showMessageDialog(null,"The Euqipment ID is null,please check it and try aganin!","",JOptionPane.INFORMATION_MESSAGE);
+
+                }
+
             break;
             case "Sterilization batch number":
-                batchNumber.setText("M2"+dateFormat2.format(date)+"-"+equipmentId.getText());
+                if(!equipmentId.getText().isEmpty()){
+                    batchNumber.setText("M2"+dateFormat2.format(date)+"-"+equipmentId.getText());}
+                else {
+                    JOptionPane.showMessageDialog(null,"The Euqipment ID is null,please check it and try aganin!","",JOptionPane.INFORMATION_MESSAGE);
+
+                }
             break;
             case "Assembly batch number":
-                batchNumber.setText("M2-"+"AS"+ dateFormat1.format(date));
+                if(!equipmentId.getText().isEmpty()){
+                    batchNumber.setText("M2-"+"AS"+ dateFormat1.format(date));}
+                else {
+                    JOptionPane.showMessageDialog(null,"The Euqipment ID is null,please check it and try aganin!","",JOptionPane.INFORMATION_MESSAGE);
+
+                }
             break;
 
         }
+
     }
 }
 
